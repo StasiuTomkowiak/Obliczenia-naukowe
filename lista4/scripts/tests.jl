22c210fbe7b521880ff1855b680166df61bda4d4
StasiuTomkowiak <tomkowiakstas@gmail.com>
2024-11-27 23:09:18 +0100

    zadanie 2 plus testy(dodac komentarze)

--- lista4/scripts/tests.jl
+++ lista4/scripts/tests.jl
@@ -6,7 +6,15 @@ using .Interpolacja
 
 
 println(ilorazyRoznicowe([0.0, 1.0], [0.0, 1.0]))
-println(ilorazyRoznicowe([1.0, 2.0, 3.0], [1.0, 4.0, 9.0]))
+println(ilorazyRoznicowe([3.0, 1.0, 5.0,6.0], [1.0, -3.0, 2.0,4.0]))
+println(ilorazyRoznicowe([-1.0, 0.0, 1.0,2.0], [2.0, 1.0, 2.0,-7.0]))
 
+println(warNewton([-1.0, 0.0, 1.0,2.0], [2.0, -1.0, 1.0, -2.0],0.0))
+println(warNewton([-1.0, 0.0, 1.0,2.0], [2.0, -1.0, 1.0, -2.0],1.0))
+println(warNewton([-1.0, 0.0, 1.0,2.0], [2.0, -1.0, 1.0, -2.0],2.0))
+println(warNewton([-1.0, 0.0, 1.0,2.0], [2.0, -1.0, 1.0, -2.0],-1.0))
+
+println(warNewton([1.0, 2.0, 3.0], [1.0, 4.0, 9.0], 2.0))
+println(warNewton([3.0, 1.0, 5.0], [1.0, -3.0, 2.0], 2.0))
 
 
